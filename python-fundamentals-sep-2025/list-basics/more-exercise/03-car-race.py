7a5226adc45d64e60c387416e08cf15df8f16a92
Valeri Elashvili <valerielashvili@gmail.com>
2025-09-30 09:29:11 +0000

    Simplify listcomp

--- python-fundamentals-sep-2025/list-basics/more-exercise/03-car-race.py
+++ python-fundamentals-sep-2025/list-basics/more-exercise/03-car-race.py
@@ -1,23 +1,22 @@
-numbers = input().split()
-numbers = [int(num) for num in numbers]
-finish_line = len(numbers) // 2 + 1
+numbers = [int(num) for num in input().split()]
+finish_line = len(numbers) // 2
 
 podracer_time = 0
 flash_speeder_time = 0
 winner_time = 0
 winner = ''
 
-for i in range(finish_line - 1):
+for i in range(finish_line):
     time = int(numbers[i])
     podracer_time += time
 
     if time == 0:
         podracer_time *= 0.8
 
-for j in range(len(numbers) - 1, finish_line - 1, -1):
+for j in range(len(numbers) - 1, finish_line, -1):
     time = int(numbers[j])
     flash_speeder_time += time
-
+    
     if time == 0:
         flash_speeder_time *= 0.8
 
