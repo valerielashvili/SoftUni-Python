23964c8394966d5bf63be25f996bd3dcbaf5d5c2
Valeri Elashvili <valerielashvili@gmail.com>
2025-09-30 09:21:29 +0000

    Add Car Race

---
+++ python-fundamentals-sep-2025/list-basics/more-exercise/03-car-race.py
@@ -0,0 +1,33 @@
+import pdb
+
+numbers = input().split()
+numbers = [int(num) for num in numbers]
+finish_line = len(numbers) // 2 + 1
+
+podracer_time = 0
+flash_speeder_time = 0
+winner_time = 0
+winner = ''
+
+for i in range(finish_line - 1):
+    time = int(numbers[i])
+    podracer_time += time
+
+    if time == 0:
+        podracer_time *= 0.8
+#pdb.set_trace()
+for j in range(len(numbers) - 1, finish_line - 1, -1):
+    time = int(numbers[j])
+    flash_speeder_time += time
+
+    if time == 0:
+        flash_speeder_time *= 0.8
+
+if podracer_time < flash_speeder_time:
+    winner = 'left'
+    winner_time = podracer_time
+else:
+    winner = 'right'
+    winner_time = flash_speeder_time
+
+print(f"The winner is {winner} with total time: {winner_time:.1f}")
