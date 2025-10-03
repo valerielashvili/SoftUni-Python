5b84205abcb3a8d1ef95220102e71a65e81ad8dd
Valeri Elashvili <valerielashvili@gmail.com>
2025-10-03 07:30:53 +0200

    Add Grades

---
+++ python-fundamentals-sep-2025/4-functions/2-grades.py
@@ -0,0 +1,19 @@
+grade_data = float(input())
+
+def print_grade(grade):
+    result = ''
+
+    if 2.00 <= grade <= 2.99:
+        result = 'Fail'
+    elif 3.00 <= grade <= 3.49:
+        result = 'Poor'
+    elif 3.50 <= grade <= 4.49:
+        result = 'Good'
+    elif 4.50 <= grade <= 5.49:
+        result = 'Very Good'
+    elif 5.50 <= grade <= 6.00:
+        result = 'Excellent'
+
+    return result
+
+print(print_grade(grade_data))
