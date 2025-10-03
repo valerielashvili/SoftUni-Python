656f1bea2dff50b48509cd9e2450bb1ed2daccd9
Valeri Elashvili <valerielashvili@gmail.com>
2025-10-04 00:55:25 +0200

    Add Min Max and Sum

---
+++ python-fundamentals-sep-2025/4-functions/exercise/7-min-max-and-sum.py
@@ -0,0 +1,9 @@
+numbers = [int(x) for x in input().split()]
+
+min_num = min(numbers)
+max_num = max(numbers)
+sum_of_nums = sum(numbers)
+
+print(f"The minimum number is {min_num}\n"
+      f"The maximum number is {max_num}\n"
+      f"The sum number is: {sum_of_nums}")
