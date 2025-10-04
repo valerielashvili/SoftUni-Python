7dbfd5d68d6772b9d7c6cbad5d16a8e009ec2190
Valeri Elashvili <valerielashvili@gmail.com>
2025-10-04 16:35:24 +0200

    Use Euclidean distance

--- python-fundamentals-sep-2025/4-functions/more-exercise/2-center-point.py
+++ python-fundamentals-sep-2025/4-functions/more-exercise/2-center-point.py
@@ -1,4 +1,4 @@
-import math
+from math import floor
 
 x_first = float(input())
 y_first = float(input())
@@ -6,13 +6,13 @@ x_second = float(input())
 y_second = float(input())
 
 def closest_to_center(x1, y1, x2, y2):
-    first_point = abs(x1) + abs(y1)
-    second_point = abs(x2) + abs(y2)
+    first_point = x1 ** 2 + y1 ** 2
+    second_point = x2 ** 2 + y2 ** 2
 
-    if first_point < second_point:
-        return f"({math.floor(x1)}, {math.floor(y1)})"
+    if first_point <= second_point:
+        return f"({floor(x1)}, {floor(y1)})"
     else:
-        return f"({math.floor(x2)}, {math.floor(y2)})"
+        return f"({floor(x2)}, {floor(y2)})"
 
 result = closest_to_center(x_first, y_first, x_second, y_second)
 print(result)
