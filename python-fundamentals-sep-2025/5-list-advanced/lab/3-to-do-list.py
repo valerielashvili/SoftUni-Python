a7e146b4763866e5975e1e5ff4d41606736fbb03
Valeri Elashvili <valerielashvili@gmail.com>
2025-10-07 23:33:19 +0200

    Add To-do List

---
+++ python-fundamentals-sep-2025/5-list-advanced/lab/3-to-do-list.py
@@ -0,0 +1,14 @@
+command = input()
+notes = [0] * 10
+
+while command != 'End':
+    tokens = command.split('-')
+    priority = int(tokens[0]) - 1
+    note = tokens[1]
+    notes.pop(priority)
+    notes.insert(priority, note)
+
+    command = input()
+
+result = [element for element in notes if element != 0]
+print(result)
