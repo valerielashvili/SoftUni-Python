ff567e389dcea9894c942fc3889f7f36d6a5af7d
Valeri Elashvili <valerielashvili@gmail.com>
2025-10-25 09:09:49 +0200

    Add Vehicle

---
+++ python-fundamentals-sep-2025/8-objects-and-classes/exercise/8-vehicle.py
@@ -0,0 +1,37 @@
+class Vehicle:
+    def __init__(self, car_type: str, model: str, price: int, owner=None):
+        self.type = car_type
+        self.model = model
+        self.price = price
+        self.owner = owner
+
+    def buy(self, money: int, owner: str):
+        if self.owner is not None:
+            return "Car already sold"
+        elif money >= self.price:
+            self.owner = owner
+            return f"Successfully bought a {self.type}. Change: {money - self.price:.2f}"
+        else:
+            return "Sorry, not enough money"
+
+    def sell(self):
+        if self.owner is not None:
+            self.owner = None
+        else:
+            return "Vehicle has no owner"
+
+    def __repr__(self):
+        if self.owner is not None:
+            return f"{self.model} {self.type} is owned by: {self.owner}"
+        else:
+            return f"{self.model} {self.type} is on sale: {self.price}"
+
+vehicle_type = "car"
+model = "BMW"
+price = 30000
+vehicle = Vehicle(vehicle_type, model, price)
+print(vehicle.buy(15000, "Peter"))
+print(vehicle.buy(35000, "George"))
+print(vehicle)
+vehicle.sell()
+print(vehicle)
