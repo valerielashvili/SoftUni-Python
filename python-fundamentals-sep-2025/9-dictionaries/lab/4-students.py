769a150b49d2b79777d49d0cba34e8587941b4df
Valeri Elashvili <valerielashvili@gmail.com>
2025-10-29 13:28:13 +0100

    refactor for nested dict

--- python-fundamentals-sep-2025/9-dictionaries/lab/4-students.py
+++ python-fundamentals-sep-2025/9-dictionaries/lab/4-students.py
@@ -3,11 +3,14 @@ students = {}
 
 while ':' in input_line:
     name, sid, course = input_line.split(':')
-    students.update({name: [sid, course]})
+    if course not in students:
+        students.update({course: {}})
+    students[course].update({name: sid})
 
     input_line = input()
 
 filter_course = input_line.replace('_', ' ')
-for student, info in students.items():
-    if filter_course in info:
-        print(f"{student} - {info[0]}")
+for course, student in students.items():
+    for name, sid in student.items():
+        if filter_course == course:
+            print(f"{name} - {sid}")
