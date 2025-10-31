5c2cccd1a247b5b7253f8427b6d79c13c4b521cd
Valeri Elashvili <valerielashvili@gmail.com>
2025-10-31 01:16:43 +0100

    Add Student Academy

---
+++ python-fundamentals-sep-2025/9-dictionaries/exercise/9-student-academy.py
@@ -0,0 +1,17 @@
+n_lines = int(input())
+students = {}
+
+for n in range(0, n_lines * 2, 2):
+    name, grade = input(), float(input())
+    grades = students.get(name, [])
+    grades.append(grade)
+    students[name] = grades
+
+filtered_students = {
+    k: average
+    for k, v in students.items()
+    if v and (average := sum(v) / len(v)) >= 4.50
+}
+
+for name, avg_grade in filtered_students.items():
+    print(f"{name} -> {avg_grade:.2f}")
