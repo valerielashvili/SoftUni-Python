72f196544fb8cc9a245cbcb0cec9624e8bcba340
Valeri Elashvili <valerielashvili@gmail.com>
2025-10-31 01:02:15 +0100

    use walrus operator

--- python-fundamentals-sep-2025/9-dictionaries/exercise/8-courses.py
+++ python-fundamentals-sep-2025/9-dictionaries/exercise/8-courses.py
@@ -1,14 +1,11 @@
 courses = {}
-input_line = input()
 
-while input_line != 'end':
+while (input_line := input()) != 'end':
     course, name = input_line.split(' : ')
     if course not in courses:
         courses[course] = [name]
     else:
         courses[course].append(name)
-
-    input_line = input()
 
 for course, students in courses.items():
     print(f"{course}: {len(students)}")
