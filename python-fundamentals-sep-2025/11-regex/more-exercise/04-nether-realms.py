923342bf85a5abdd3dc477367f43fcdd83c8f12c
Valeri Elashvili <valerielashvili@gmail.com>
2025-11-16 15:29:09 +0100

    take input separately

--- python-fundamentals-sep-2025/11-regex/more-exercise/04-nether-realms.py
+++ python-fundamentals-sep-2025/11-regex/more-exercise/04-nether-realms.py
@@ -1,11 +1,13 @@
 import re
 
 delimit = re.compile(r'\s*,\s*')
+demon_names = delimit.split(input())
+
 health_regex = re.compile(r'[^0-9+\-*/.]')
 damage_regex = re.compile(r'[-+]?[0-9]+(?:\.[0-9]+)?')
 op_rx = re.compile(r'[*/]')
 
-demon_names = {d: {'health': 0, 'damage': 0.0} for d in delimit.split(input())}
+demons = {}
 
 for name in demon_names:
     total_health = sum(ord(c) for c in health_regex.findall(name))
@@ -18,9 +20,9 @@ for name in demon_names:
         elif op == '/':
             base_damage /= 2
 
-    demon_names[name] = {'health': total_health, 'damage': base_damage}
+    demons[name] = {'health': total_health, 'damage': base_damage}
 
-sorted_names = dict(sorted(demon_names.items()))
+sorted_names = dict(sorted(demons.items()))
 
 for name, value in sorted_names.items():
     print(f"{name} - {value['health']} health, {value['damage']:.2f} damage")
