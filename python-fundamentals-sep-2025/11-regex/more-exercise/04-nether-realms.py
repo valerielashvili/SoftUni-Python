a841942787c6984913f50afaa21ccacb2e5a230e
Valeri Elashvili <valerielashvili@gmail.com>
2025-11-16 15:22:33 +0100

    Add Nether Realms

---
+++ python-fundamentals-sep-2025/11-regex/more-exercise/04-nether-realms.py
@@ -0,0 +1,26 @@
+import re
+
+delimit = re.compile(r'\s*,\s*')
+health_regex = re.compile(r'[^0-9+\-*/.]')
+damage_regex = re.compile(r'[-+]?[0-9]+(?:\.[0-9]+)?')
+op_rx = re.compile(r'[*/]')
+
+demon_names = {d: {'health': 0, 'damage': 0.0} for d in delimit.split(input())}
+
+for name in demon_names:
+    total_health = sum(ord(c) for c in health_regex.findall(name))
+    base_damage = sum(float(n) for n in damage_regex.findall(name))
+    operators = op_rx.findall(name)
+
+    for op in operators:
+        if op == '*':
+            base_damage *= 2
+        elif op == '/':
+            base_damage /= 2
+
+    demon_names[name] = {'health': total_health, 'damage': base_damage}
+
+sorted_names = dict(sorted(demon_names.items()))
+
+for name, value in sorted_names.items():
+    print(f"{name} - {value['health']} health, {value['damage']:.2f} damage")
