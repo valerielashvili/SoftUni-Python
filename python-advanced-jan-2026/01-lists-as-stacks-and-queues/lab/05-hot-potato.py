383d3626483a53d5db01d411b2f98b13913a9f8a
Valeri Elashvili <valerielashvili@gmail.com>
2026-01-07 00:08:47 +0100

    rm blank line

--- python-advanced-jan-2026/01-lists-as-stacks-and-queues/lab/05-hot-potato.py
+++ python-advanced-jan-2026/01-lists-as-stacks-and-queues/lab/05-hot-potato.py
@@ -7,7 +7,6 @@ toss_num = int(input())
 while len(children) != 1:
     for i in range(toss_num):
         children.append(children.popleft())
-
     print(f"Removed {children.pop()}")
 
 print(f"Last is {children[0]}")
