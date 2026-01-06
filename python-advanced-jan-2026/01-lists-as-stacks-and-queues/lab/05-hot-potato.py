b3445204754baec24410ced2096ac3cbbcdc4e09
Valeri Elashvili <valerielashvili@gmail.com>
2026-01-07 00:07:56 +0100

    Add Hot Potato

---
+++ python-advanced-jan-2026/01-lists-as-stacks-and-queues/lab/05-hot-potato.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+
+children = deque(input().split())
+toss_num = int(input())
+
+while len(children) != 1:
+    for i in range(toss_num):
+        children.append(children.popleft())
+
+    print(f"Removed {children.pop()}")
+
+print(f"Last is {children[0]}")
