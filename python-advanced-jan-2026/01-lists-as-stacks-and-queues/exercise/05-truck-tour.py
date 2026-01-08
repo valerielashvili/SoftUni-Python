c0fafdd53cfb1d16d110d807e6f752bbb144d6cf
Valeri Elashvili <valerielashvili@gmail.com>
2026-01-09 00:28:56 +0100

    correct logic

--- python-advanced-jan-2026/01-lists-as-stacks-and-queues/exercise/05-truck-tour.py
+++ python-advanced-jan-2026/01-lists-as-stacks-and-queues/exercise/05-truck-tour.py
@@ -1,14 +1,11 @@
 n = int(input())
 pumps = [list(map(int, input().split())) for _ in range(n)]
 
-tank = 0
-start = 0
-total_fuel = 0
+tank, start = 0, 0
 
 for i in range(n):
     fuel, distance = pumps[i]
     tank += fuel - distance
-    total_fuel += fuel - distance
 
     if tank < 0:
         start = i + 1
