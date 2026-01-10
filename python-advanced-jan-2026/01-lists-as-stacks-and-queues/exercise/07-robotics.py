dc72d194bea310e331e033a28dd0168abfec1003
Valeri Elashvili <valerielashvili@gmail.com>
2026-01-10 17:44:36 +0100

    Add Robotics

---
+++ python-advanced-jan-2026/01-lists-as-stacks-and-queues/exercise/07-robotics.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+
+robots = [
+    {'name': name, 'process_time': int(time)}
+    for name, time in (robot.split('-') for robot in input().split(';'))
+]
+h, m, s = map(int, input().split(':'))
+current_time = h * 3600 + m * 60 + s
+products = deque([])
+
+for robot in robots:
+    robot.update({'next_free_time': current_time})
+
+while (product := input()) != 'End':
+    products.append(product)
+
+while products:
+    current_time += 1
+    product = products.popleft()
+
+    for robot in robots:
+        if robot['next_free_time'] <= current_time:
+            robot['next_free_time'] = current_time + robot['process_time']
+
+            t = current_time % (24 * 3600)
+            h, m, s = t // 3600, (t % 3600) // 60, t % 60
+            print(f"{robot['name']} - {product} [{h:02d}:{m:02d}:{s:02d}]")
+            break
+    else:
+        products.append(product)
