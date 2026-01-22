6fbec9672c8ca528012bc4fffb01909036f780a3
Valeri Elashvili <valerielashvili@gmail.com>
2026-01-22 14:59:57 +0100

    finalize Miner

--- python-advanced-jan-2026/04-multidimensional-lists/exercise/09-miner.py
+++ python-advanced-jan-2026/04-multidimensional-lists/exercise/09-miner.py
@@ -1,4 +1,8 @@
+import sys
+
+
 def move(direction, mtrx_size, r, c):
+    last_r, last_c = r, c
     if direction == 'up':
         r -= 1
     elif direction == 'down':
@@ -7,11 +11,10 @@ def move(direction, mtrx_size, r, c):
         c -= 1
     elif direction == 'right':
         c += 1
-    if 0 <= r <= mtrx_size and 0 <= c <= mtrx_size:
+    if 0 <= r < mtrx_size and 0 <= c < mtrx_size:
         return r, c
     else:
-        return False
-
+        return last_r, last_c
 
 size = int(input())
 commands = [
@@ -23,33 +26,26 @@ matrix = []
 collected_coal = 0
 game_over = False
 
-for row in range(size):
+for r in range(size):
     matrix.append([c for c in input().split()])
 
-current_pos = tuple((r, c) for r in range(size) for c in range(size) if matrix[r][c] == 's')
-last_r, last_c = None, None
+row, col = next(
+    ((r, c) for r in range(size) for c in range(size) if matrix[r][c] == 's'),
+    None
+)
 
 for cmd in commands:
-    if not move(cmd, size, *current_pos):
-        continue
-
-    row, col = move(cmd, size, *current_pos)
-    last_r, last_c = row, col
-
+    row, col = move(cmd, size, row, col)
 
     if matrix[row][col] == 'c':
-        collected_coal += 1
         matrix[row][col] = '*'
-    elif 'c' not in matrix:
-        print(f'You collected all coal! ({row}, {col})')
-        game_over = True
-        break
     elif matrix[row][col] == 'e':
         print(f'Game over! ({row}, {col})')
-        game_over = True
-        break
+        sys.exit()
 
-remaining_coal = sum(matrix[r][c] == 'c' for r in matrix for c in r)
+coal = sum(col == 'c' for row in matrix for col in row)
 
-if not game_over:
-    print(f'{remaining_coal} pieces of coal left. ({last_r}, {last_c})')
+if not coal:
+    print(f'You collected all coal! ({row}, {col})')
+else:
+    print(f'{coal} pieces of coal left. ({row}, {col})')
