044d05f3746568543e60d233d42c5f131db2bcac
Valeri Elashvili <valerielashvili@gmail.com>
2026-02-03 20:43:19 +0100

    Add ASCII Art

---
+++ python-advanced-jan-2026/09-modules/lab/02-ascii-art.py
@@ -0,0 +1,9 @@
+from pyfiglet import figlet_format
+
+
+def print_art(msg):
+    ascii_art = figlet_format(msg)
+    print(ascii_art)
+
+message = input("What would you like to print? ")
+print_art(message)
