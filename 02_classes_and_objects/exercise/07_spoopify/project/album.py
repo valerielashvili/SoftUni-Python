e463af2068a5d67b75f944f582eebe6ed1e957da
Valeri Elashvili <valerielashvili@gmail.com>
2026-02-22 23:21:18 +0100

    Add Spoopify

---
+++ 02_classes_and_objects/exercise/07_spoopify/project/album.py
@@ -0,0 +1,41 @@
+from song import *
+
+
+class Album:
+    def __init__(self, name: str, *args):
+        self.name = name
+        self.published = False
+        self.songs = list(args)
+
+    def add_song(self, song: Song):
+        if self.published:
+            return "Cannot add songs. Album is published."
+        elif song.single:
+            return f"Cannot add {song.name}. It's a single"
+        elif song in self.songs:
+            return "Song is already in the album."
+        else:
+            self.songs.append(song)
+            return f"Song {song.name} has been added to the album {self.name}."
+
+    def remove_song(self, song_name: str):
+        if self.published:
+            return "Cannot remove songs. Album is published."
+
+        for song in self.songs:
+            if song.name == song_name:
+                self.songs.remove(song)
+                return f"Removed song {song_name} from album {self.name}."
+
+        return "Song is not in the album."
+
+    def publish(self):
+        if self.published:
+            return f"Album {self.name} is already published."
+        else:
+            self.published = True
+            return f"Album {self.name} has been published."
+
+    def details(self):
+        songs_info = '\n'.join(f"== {s.get_info()}" for s in self.songs)
+        return f"Album {self.name}\n{songs_info}"
