3baab13dd8e79189db669637c1061f702497775b
Valeri Elashvili <valerielashvili@gmail.com>
2026-03-02 00:29:35 +0100

    Add Shop

---
+++ python-oop-feb-2026/03_inheritance/exercise/05_shop/main.py
@@ -0,0 +1,15 @@
+from food import Food
+from drink import Drink
+from product_repository import ProductRepository
+
+
+# Test code
+food = Food("apple")
+drink = Drink("water")
+repo = ProductRepository()
+repo.add(food)
+repo.add(drink)
+print(repo.products)
+print(repo.find("water"))
+repo.find("apple").decrease(5)
+print(repo)
---
+++ python-oop-feb-2026/03_inheritance/exercise/05_shop/product_repository.py
@@ -0,0 +1,20 @@
+from product import Product
+
+
+class ProductRepository:
+    def __init__(self):
+        self.products = []
+
+    def add(self, product: Product):
+        self.products.append(product)
+
+    def find(self, product_name: str):
+        return next((p for p in self.products if p.name == product_name), None)
+
+    def remove(self, product_name):
+        product = self.find(product_name)
+        if product:
+            self.products.remove(product)
+
+    def __repr__(self):
+        return '\n'.join(f"{p.name}: {p.quantity}" for p in self.products)
