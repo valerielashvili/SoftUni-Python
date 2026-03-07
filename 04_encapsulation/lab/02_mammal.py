541fdfd7664efd2efadc0efbc68b7975c35b1d8a
Valeri Elashvili <valerielashvili@gmail.com>
2026-03-07 15:01:11 +0100

    Correct Mammal

--- 04_encapsulation/lab/02_mammal.py
+++ 04_encapsulation/lab/02_mammal.py
@@ -8,9 +8,9 @@ class Mammal:
     
     def make_sound(self):
         return f"{self.name} makes {self.sound}"
-    
+
     def get_kingdom(self):
         return self.__kingdom
-    
+
     def info(self):
         return f"{self.name} is of type {self.type}"
