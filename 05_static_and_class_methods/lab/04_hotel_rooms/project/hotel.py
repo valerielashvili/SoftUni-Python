da49b92d19118ed3382f7286affa122b33042586
Valeri Elashvili <valerielashvili@gmail.com>
2026-03-08 11:39:36 +0100

    Add Hotel Rooms

---
+++ 05_static_and_class_methods/lab/04_hotel_rooms/project/hotel.py
@@ -0,0 +1,34 @@
+from project.room import Room
+
+
+class Hotel:
+    def __init__(self, name: str):
+        self.name = name
+        self.rooms = []
+        self.guests = 0
+
+    @classmethod
+    def from_stars(cls, stars_count: int):
+        return cls(f"{stars_count} stars Hotel")
+
+    def add_room(self, room: Room):
+        return self.rooms.append(room)
+
+    def take_room(self, room_number, people):
+        room = next((r for r in self.rooms if r.number == room_number), None)
+        if not room.is_taken:
+            room.take_room(people)
+            if room.is_taken:
+                self.guests += people
+
+    def free_room(self, room_number):
+        room = next((r for r in self.rooms if r.number == room_number), None)
+        if not room.is_taken:
+            room_guests = room.guests
+            room.free_room()
+            self.guests -= room_guests
+
+    def status(self):
+        return (f"Hotel {self.name} has {self.guests} total guests\n"
+                f"Free rooms: {', '.join(str(r.number) for r in self.rooms if not r.is_taken)}\n"
+                f"Taken rooms: {', '.join(str(r.number) for r in self.rooms if r.is_taken)}")
