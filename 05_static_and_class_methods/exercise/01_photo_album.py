2fec2df4db634bd2b0c5a23eb2d3ad8697242dd4
Valeri Elashvili <valerielashvili@gmail.com>
2026-03-08 23:33:25 +0100

    Add class constant

--- 05_static_and_class_methods/exercise/01_photo_album.py
+++ 05_static_and_class_methods/exercise/01_photo_album.py
@@ -1,18 +1,21 @@
+from math import ceil
+
+
 class PhotoAlbum:
+    TOTAL_PAGES = 4
+
     def __init__(self, pages: int):
         self.pages = pages
-        self.photos = []
-        for row in range(pages):
-            self.photos.append([])
+        self.photos = [[] for _ in range(pages)]
 
     @classmethod
     def from_photos_count(cls, photos_count: int):
-        num_pages = photos_count // 4 + photos_count % 4
+        num_pages = ceil(photos_count / cls.TOTAL_PAGES)
         return cls(num_pages)
 
     def add_photo(self, label: str):
         for i, r in enumerate(self.photos):
-            if len(r) < 4:
+            if len(r) < PhotoAlbum.TOTAL_PAGES:
                 r.append(label)
                 return f"{label} photo added successfully on page {i + 1} slot {len(r)}"
         return "No more free slots"
@@ -22,7 +25,7 @@ class PhotoAlbum:
         for r in self.photos:
             output += (f"-----------\n"
                        f"{'[] ' * len(r)}".rstrip() + "\n")
-            if len(r) == 0:
+            if not r:
                 output += "\n"
         output += "-----------"
         return output
