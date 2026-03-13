caf22ce7f2618d3a12832d77b88fc1b7fe6e9ba1
Valeri Elashvili <valerielashvili@gmail.com>
2026-03-13 14:07:03 +0100

    Add Vehicle

---
+++ 06_polymorphism_and_abstraction/exercise/01_vehicle.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+
+
+class Vehicle(ABC):
+    def __init__(self, fuel_quantity: int, fuel_consumption: int):
+        self.fuel_quantity = fuel_quantity
+        self.fuel_consumption = fuel_consumption
+
+    @abstractmethod
+    def drive(self, distance):
+        pass
+
+    @abstractmethod
+    def refuel(self, fuel):
+        pass
+
+
+class Car(Vehicle):
+    CONSUMPT_CORRECTION = 0.9
+
+    def drive(self, distance):
+        consumption = distance * (self.fuel_consumption + Car.CONSUMPT_CORRECTION)
+        if self.fuel_quantity >= consumption:
+            self.fuel_quantity -= consumption
+
+    def refuel(self, fuel):
+        self.fuel_quantity += fuel
+
+
+class Truck(Vehicle):
+    CONSUMPT_CORRECTION = 1.6
+
+    def drive(self, distance):
+        consumption = distance * (self.fuel_consumption + Truck.CONSUMPT_CORRECTION)
+        if self.fuel_quantity >= consumption:
+            self.fuel_quantity -= consumption
+
+    def refuel(self, fuel):
+        # Keeps only 95% of the given fuel
+        self.fuel_quantity += fuel * 0.95
+
+
+car = Car(20, 5)
+car.drive(3)
+print(car.fuel_quantity)
+car.refuel(10)
+print(car.fuel_quantity)
+
+truck = Truck(100, 15)
+truck.drive(5)
+print(truck.fuel_quantity)
+truck.refuel(50)
+print(truck.fuel_quantity)
