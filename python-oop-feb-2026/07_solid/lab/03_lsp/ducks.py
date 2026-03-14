94cdf342ec6fe865c0d7a90a9c5703b8aa92f0a5
Valeri Elashvili <valerielashvili@gmail.com>
2026-03-14 22:23:53 +0100

    Add Ducks

---
+++ python-oop-feb-2026/07_solid/lab/03_lsp/ducks.py
@@ -0,0 +1,42 @@
+from abc import ABC
+
+
+class Toy(ABC):
+    """Class Duck refactored to Toy"""
+    @staticmethod
+    def quack():
+        pass
+
+    @staticmethod
+    def walk():
+        pass
+
+    @staticmethod
+    def fly():
+        pass
+
+
+class RobotDuck(Toy):
+    HEIGHT = 50
+
+    def __init__(self):
+        self.height = 0
+
+    @staticmethod
+    def quack():
+        return 'Robotic quacking'
+
+    @staticmethod
+    def walk():
+        return 'Robotic walking'
+
+    def fly(self):
+        """can only fly to specific height but
+        when it reaches it starts landing automatically"""
+        if self.height == RobotDuck.HEIGHT:
+            self.land()
+        else:
+            self.height += 1
+
+    def land(self):
+        self.height = 0
