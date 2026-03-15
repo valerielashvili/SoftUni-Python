f088bab4eb20fee2efb9e5c7246d91a4ea5d2b6a
Valeri Elashvili <valerielashvili@gmail.com>
2026-03-15 12:09:16 +0100

    Add Emails

---
+++ python-oop-feb-2026/07_solid/exercise/05_emails.py
@@ -0,0 +1,66 @@
+"""Emails module respects Single Responsibility Principle."""
+
+from abc import ABC, abstractmethod
+
+
+class IContent(ABC):
+    def __init__(self, content):
+        self.content = content
+
+    @abstractmethod
+    def format(self):
+        pass
+
+
+class MyContent(IContent):
+    def format(self):
+        return f"<MyML>{self.content}</MyML>\n"
+
+
+class IEmail(ABC):
+    @abstractmethod
+    def set_sender(self, sender):
+        pass
+
+    @abstractmethod
+    def set_receiver(self, receiver):
+        pass
+
+    @abstractmethod
+    def set_content(self, content):
+        pass
+
+class Email(IEmail):
+
+    def __init__(self, protocol):
+        self.protocol = protocol
+        self.__sender = None
+        self.__receiver = None
+        self.__content = None
+
+    def set_sender(self, sender):
+        if self.protocol == 'IM':
+            self.__sender = ''.join(["I'm ", sender])
+        else:
+            self.__sender = sender
+
+    def set_receiver(self, receiver):
+        if self.protocol == 'IM':
+            self.__receiver = ''.join(["I'm ", receiver])
+        else:
+            self.__receiver = receiver
+
+    def set_content(self, content):
+        self.__content = content.format()
+
+    def __repr__(self):
+        return f"Sender: {self.__sender}\nReceiver: {self.__receiver}\nContent:\n{self.__content}"
+
+
+# Test code
+email = Email('IM')
+email.set_sender('qmal')
+email.set_receiver('james')
+content = MyContent('Hello, there!')
+email.set_content(content)
+print(email)
