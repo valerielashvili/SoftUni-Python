7dd24469e95abc0460765bec216c23e9d8b24cda
Valeri Elashvili <valerielashvili@gmail.com>
2026-03-16 23:32:34 +0100

    Add Countdown Iterator

---
+++ 08_iterators_and_generators/exercise/03_countdown_iterator.py
@@ -0,0 +1,21 @@
+# Judge test requirement to have a snake_case class name
+class countdown_iterator:
+    def __init__(self, count: int):
+        self.count = count
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.count < 0:
+            raise StopIteration
+
+        cnt = self.count
+        self.count -= 1
+        return cnt
+
+
+# Test code
+iterator = countdown_iterator(10)
+for item in iterator:
+    print(item, end=" ")
