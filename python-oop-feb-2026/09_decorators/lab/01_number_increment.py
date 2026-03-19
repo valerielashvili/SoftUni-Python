e9b41f5f33048cc920b91c5d2084d24f23740dc2
Valeri Elashvili <valerielashvili@gmail.com>
2026-03-19 18:11:34 +0100

    Use list comp

--- python-oop-feb-2026/09_decorators/lab/01_number_increment.py
+++ python-oop-feb-2026/09_decorators/lab/01_number_increment.py
@@ -1,8 +1,6 @@
 def number_increment(numbers):
     def increase():
-        for i in range(len(numbers)):
-            numbers[i] += 1
-        return numbers
+        return [x + 1 for x in numbers]
     
     return increase()
 
