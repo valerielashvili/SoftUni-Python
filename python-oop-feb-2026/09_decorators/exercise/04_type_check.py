4855689f8501aff580a8751e8d3c83515d39b14c
Valeri Elashvili <valerielashvili@gmail.com>
2026-03-20 14:43:13 +0100

    Add Type Check

---
+++ python-oop-feb-2026/09_decorators/exercise/04_type_check.py
@@ -0,0 +1,23 @@
+def type_check(data_type):
+    def decorator(func):
+        def wrapper(param):
+            if data_type is type(param):
+                return func(param)
+            else:
+                return "Bad Type"
+        return wrapper
+    return decorator
+
+# Test code
+@type_check(int)
+def times2(num):
+    return num*2
+print(times2(2))
+print(times2('Not A Number'))
+
+@type_check(str)
+def first_letter(word):
+    return word[0]
+
+print(first_letter('Hello World'))
+print(first_letter(['Not', 'A', 'String']))
